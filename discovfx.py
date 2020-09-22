20cc1e0a487f82e1f78a4d901b9cd0378e7bc71e
Tommi Enenkel <tommi@Tommis-iMac-2.local>
2020-09-22 23:11:58 +0200

    add PlacePrekeyOperation

--- discovfx.py
+++ discovfx.py
@@ -14,11 +14,37 @@ bl_info = {
 import bpy
 import random
 import mathutils
+import math
+
+
+def image_sequence_resolve_all(filepath):
+    import os
+
+    basedir, filename = os.path.split(filepath)
+    filename_noext, ext = os.path.splitext(filename)
+
+    from string import digits
+    if isinstance(filepath, bytes):
+        digits = digits.encode()
+    filename_nodigits = filename_noext.rstrip(digits)
+
+    if len(filename_nodigits) == len(filename_noext):
+        # input isn't from a sequence
+        return []
+
+    return [
+        f.path
+        for f in os.scandir(basedir)
+        if f.is_file() and
+           f.name.startswith(filename_nodigits) and
+           f.name.endswith(ext) and
+           f.name[len(filename_nodigits):-len(ext) if ext else -1].isdigit()
+    ]
 
 
 class DiscoProps(bpy.types.PropertyGroup):
     sourcePath: bpy.props.StringProperty(subtype="FILE_PATH", default="/Users/tommi/Downloads/Wait a minute who are you Greenscreen Kazoo Kid Me.mp4")   
-    targetPath: bpy.props.StringProperty(subtype="FILE_PATH", default="/Users/tommi/Documents/Blender/Renders/tmp/") 
+    prekeyPath: bpy.props.StringProperty(subtype="FILE_PATH", default="/Users/tommi/Documents/Blender/Renders/tmp/") 
 
 
 class DISCO_PT_main_panel(bpy.types.Panel):
@@ -32,92 +58,29 @@ class DISCO_PT_main_panel(bpy.types.Panel):
     def draw(self, context):
         props = context.window_manager.disco_props
         layout = self.layout
-        box = layout.box()
-        box.label(text = "Compositing")
-        box.prop(props, "sourcePath")
-        box.prop(props, "targetPath")
-        box.prop(props, "color")
-        box.operator("disco.prekey")
-        layout.label(text = "Playground")
-        layout.operator("disco.delete_all_objects")
-        layout.operator("disco.add_5x5")
-        layout.operator("disco.add_color_material")
-        layout.operator("disco.add_random_material")
-
-
-class AddColorMaterialOperator(bpy.types.Operator):
-    bl_label = "Add Color Shader"
-    bl_idname = "disco.add_color_material"
+        layout.label(text = "Compositing")
+        layout.prop(props, "sourcePath")
+        layout.prop(props, "prekeyPath")
+        layout.operator("disco.prepare_prekey")
+        layout.operator("disco.render_prekey")
+        layout.operator("disco.place_prekey")
+        layout.label(text = "Housekeeping")
+        layout.operator("disco.clear_scene")
     
-    color: bpy.props.FloatVectorProperty(name= "Color", subtype= 'COLOR_GAMMA' , size=4, default= (0,0,0,1))
-
-    
-    def execute(self, context):
-        
-        # Create Material
-        material_basic = bpy.data.materials.new(name= "Disco")
-        material_basic.use_nodes = True
-        
-        # Principled BSDF
-        principled_node = material_basic.node_tree.nodes.get('Principled BSDF')
-        principled_node.inputs[7].default_value = 0.08
-        
-        # Color Node
-        rgb_node = material_basic.node_tree.nodes.new('ShaderNodeRGB')
-        rgb_node.location = (-250, 0)
-        rgb_node.outputs[0].default_value = self.color
-        
-        # Connect it
-        link = material_basic.node_tree.links.new
-        link(rgb_node.outputs[0], principled_node.inputs[0])
-        
-        # Assign the material
-        bpy.context.object.active_material = material_basic
-    
-        return {'FINISHED'}
-    
-    def invoke(self, context, event):
-        return context.window_manager.invoke_props_dialog(self)
-
-
-class AddRandomMaterialOperator(bpy.types.Operator):
-    bl_label = "Add Random Shader"
-    bl_idname = "disco.add_random_material"
-    
-    def execute(self, context):
-        c = mathutils.Color()
-        c.hsv = random.random(), random.uniform(0.9, 1), 1
-        bpy.ops.disco.add_color_material(color = (c.r, c.g, c.b, 1))
-        return {'FINISHED'}
-    
-
-class Generate5x5Operator(bpy.types.Operator):
-    bl_label = "Add 5x5"
-    bl_idname = "disco.add_5x5"    
-    
-    def execute(self, context):
-        for x in range(5):
-            for y in range(5):
-                location = (x, y, 0)
-                bpy.ops.mesh.primitive_plane_add(size = 1, location = location)
-                bpy.ops.object.select_all(action='SELECT')
-                bpy.ops.disco.add_random_material()
-        bpy.ops.view3d.view_selected()    
-        return {'FINISHED'}
-    
-class DeleteAllObjectsOperator(bpy.types.Operator):
-    bl_label = "Delete All Objects"
-    bl_idname = "disco.delete_all_objects"    
+class ClearSceneOperator(bpy.types.Operator):
+    bl_label = "Clear Scene"
+    bl_idname = "disco.clear_scene"    
     
     def execute(self, context):
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
+        bpy.ops.outliner.orphans_purge()
         return {'FINISHED'}
 
 
-class PrekeyOperator(bpy.types.Operator):
-    bl_label = "Create Prekeyed Cards"
-    bl_idname = "disco.prekey" 
+class PreparePrekeyOperator(bpy.types.Operator):
+    bl_label = "Prepare Prekeyed Card"
+    bl_idname = "disco.prepare_prekey" 
     
     def execute(self, context):
         props = context.window_manager.disco_props
@@ -155,20 +118,87 @@ class PrekeyOperator(bpy.types.Operator):
         # Render the images 
         scene.render.resolution_x = image.size[0]
         scene.render.resolution_y = image.size[1]
-        scene.render.filepath = props.targetPath
+        scene.render.filepath = props.prekeyPath
         #bpy.ops.render.render(write_still = True)
+        
+        return {'FINISHED'}
+    
+    
+class RenderPrekeyOperator(bpy.types.Operator):
+    bl_label = "Render Prekeyed Card"
+    bl_idname = "disco.render_prekey" 
+    
+    def execute(self, context):
+        scene = bpy.data.scenes.new(name='Prekey')
+        bpy.context.window.scene = scene
+        bpy.ops.render.render(write_still = True)
+        
+        return {'FINISHED'}
+
+class PlacePrekeyOperator(bpy.types.Operator):
+    bl_label = "Place Prekeyed Card"
+    bl_idname = "disco.place_prekey" 
+    
+    def execute(self, context):
+        props = context.window_manager.disco_props
+        
+        # Load Image
+        image_files = image_sequence_resolve_all(props.prekeyPath + "0.png")
+        image = bpy.data.images.load(filepath = image_files[0], check_existing=True)
+        image.source = "SEQUENCE"
+
+        # Create an object
+        bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))        
+        plane = bpy.context.active_object
+        px, py = image.size
+        y = 1
+        x = px / py * y
+        plane.dimensions = x, y, 0.0
+        plane.rotation_euler.x = math.pi/2
+        
+        # Create the material
+        material = bpy.data.materials.new(name="Card")
+        material.blend_method = "BLEND"
+        material.use_nodes = True
+        plane.active_material = material        
+        # Prepare editing the node network
+        nodes = plane.active_material.node_tree.nodes
+        links = plane.active_material.node_tree.links
+        nodes.remove(nodes["Principled BSDF"])
+        materialOutputNode = nodes["Material Output"]  
+        # Add Image Texture
+        imageTextureNode = nodes.new("ShaderNodeTexImage")
+        imageTextureNode.location = (-500, 400)
+        imageTextureNode.image = image
+        imageTextureNode.image_user.frame_duration = len(image_files)
+        imageTextureNode.image_user.use_auto_refresh = True
+        bpy.context.scene.frame_end = max(bpy.context.scene.frame_end, image.frame_duration)        
+        # Emission Shader
+        emissionNode = nodes.new("ShaderNodeEmission")
+        emissionNode.location = (-250, 0)
+        # Transparent BSDF
+        transparentNode = nodes.new("ShaderNodeBsdfTransparent")
+        transparentNode.location = (-250, 400)
+        # Mix Shader
+        mixNode = nodes.new("ShaderNodeMixShader")
+        mixNode.location = (0, 400)
+        
+        links.new(imageTextureNode.outputs[0], emissionNode.inputs[0])
+        links.new(imageTextureNode.outputs[1], mixNode.inputs[0])
+        links.new(transparentNode.outputs[0], mixNode.inputs[1])
+        links.new(emissionNode.outputs[0], mixNode.inputs[2])
+        links.new(mixNode.outputs[0], materialOutputNode.inputs[0])
         
         return {'FINISHED'}
 
 
 classes = [
     DiscoProps,
-    DISCO_PT_main_panel, 
-    AddColorMaterialOperator, 
-    AddRandomMaterialOperator, 
-    Generate5x5Operator, 
-    DeleteAllObjectsOperator,
-    PrekeyOperator]
+    DISCO_PT_main_panel,
+    ClearSceneOperator,
+    PreparePrekeyOperator,
+    RenderPrekeyOperator,
+    PlacePrekeyOperator]
  
  
  
